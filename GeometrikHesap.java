c687c8e4bb0710a88da33be8a063f982513dca2f
Amine Ceren Yiğit <aminecyigit@gmail.com>
2025-10-26 20:57:09 +0300

    Update comments to clarify program purpose
    Added a description of the program's functionality.

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -3,4 +3,6 @@
 * Öğrenci No: 250541048
 * Tarih: 26 Ekim 2025
 * Açıklama: Görev-2 Geometrik hesaplayıcı 
+
+* Bu program kullancıdan belirli verileri alır ve kullanıcının istediği hesaplamaları matematiksel olarak hesaplar.  
   
