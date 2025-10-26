de80bb35d7447c8fb743b2dc478967e35f6b3f20
Amine Ceren Yiğit <aminecyigit@gmail.com>
2025-10-26 20:47:56 +0300

    OgrenciBilgi.java
    Updated student information program to collect and display student details including name, student number, age, and GPA.

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: Amine Ceren Yiğit
+ * Ogrenci No: 250541048
+ * Tarih: 26 Ekim 2025
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class OgrenciBilgi {
     public static void main(String[] args) {
         // Scanner objesi olusturun
+
         
         
         // Degisken tanimlamalari
@@ -45,7 +46,38 @@ public class OgrenciBilgi {
         System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
         
         // COZUMUNUZU BURAYA YAZIN
-        
+        import java.util.Scanner;
+
+public class OgrenciBilgi {
+
+        public static void main(String[] args) {
+
+            // degiskeler aliniyor
+            Scanner input = new Scanner(System.in);
+            System.out.print("Adınızı girin: ");
+            String ad = input.nextLine();
+
+            System.out.print("Soyadınızı girin: ");
+            String soyad = input.nextLine();
+
+            System.out.print("Öğrenci Numaranız: ");
+            int ogrenci_no = input.nextInt();
+
+            System.out.print("Yasınız: ");
+            int yas = input.nextInt();
+
+            System.out.print("GPA (0.00-4.00):");
+            float gpa = input.nextFloat();
+
+            //bilgiler ekrana bastiriliyor
+            System.out.println("=== OGRENCI BILGI SISTEMI ===");
+            System.out.printf("Ad Soyad: %s %s  ", ad, soyad);
+            System.out.printf("\nOgrenci No: %d", ogrenci_no);
+            System.out.printf("\nYas: %d", yas);
+            System.out.printf("\nGPA %.2f", gpa);
+    }
+}
+
         
         
         
@@ -56,5 +88,4 @@ public class OgrenciBilgi {
         
         // Scanner'i kapatin (önemli pratik)
         
-    }
-}
+
