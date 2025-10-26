5ebeb8878503a453b5a5db2faac37f0e50005fd4
Amine Ceren Yiğit <aminecyigit@gmail.com>
2025-10-26 21:05:43 +0300

    Implement geometric calculations for circle and sphere
    This program calculates geometric properties based on user input.

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -6,3 +6,36 @@
 *
 * Bu program kullancıdan belirli verileri alır ve kullanıcının istediği hesaplamaları matematiksel olarak hesaplar.  
 */  
+import java.util.Scanner;
+
+public class GeometrikHesap {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    //yaricap bilgisi al
+    System.out.println("Daire yaricapini giriniz (cm):");
+    double yaricap = input.nextDouble();
+
+    //pi sayisi tanimlama
+    float pi = 3.14159f;
+    
+    //Matematiksel islemler
+    System.out.println("SONUCLAR");
+    System.out.println("----------------");
+
+    double alan = pi * yaricap * yaricap;
+    System.out.printf("Daire alanı: %.2f cm^2", alan);
+
+    double cevre = 2 * pi * yaricap;
+    System.out.printf("\nDaire cevre: %.2f cm ", cevre);
+
+    double cap = 2 * yaricap;
+    System.out.printf("\nDaire cap: %.2f cm ", cap);
+
+    double kure_hacmi = (4.0/3.0) * pi * yaricap * yaricap * yaricap;
+    System.out.printf("\nKüre hacmi: %.2f cm^3 ", kure_hacmi);
+
+    double kure_yuzey_alanı =4 * pi * Math.pow(yaricap,2);
+    System.out.printf("\nKüre Yüzey Alanı: %.2f cm^2", kure_yuzey_alanı);
+    }
+}
