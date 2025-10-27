047cab5941605568dc836f3277d9616f208ba15c
Amine Ceren Yiğit <aminecyigit@gmail.com>
2025-10-27 22:40:12 +0300

    Add header comments to MaasHesap.java
    Added header comments with author information and program description.

--- MaasHesap.java
+++ MaasHesap.java
@@ -1,3 +1,11 @@
+/*
+* Ad-Soyad: Amine Ceren Yiğit 
+* Öğrenci No: 250541048
+* Tarih: 26 Ekim 2025
+* Açıklama: Görev-2 Geometrik hesaplayıcı 
+*
+* Bu program kullancıdan belirli verileri alır ve kullanıcının istediği hesaplamaları matematiksel olarak hesaplar.  
+*/ 
 
 import java.util.Scanner;
 public class MaasHesap {
